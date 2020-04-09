6ad396d47f5e0bb2040a7c82bdfb0397c594a677
Adrien Burgun <adrien.burgun@orange.fr>
2020-04-09 17:18:35 +0200

    :sparkles: 13.4

---
+++ src/13/13.4/main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+size_t my_strlen(char* str);
+
+int main(int argc, char** argv) {
+  char* strings[1024] = {
+    "bread",
+    "The night sky is bright.",
+    "Are we there yet?",
+    "🐺"
+  };
+
+  for (size_t n = 0; n < 4; n++) {
+    printf("'%s' is %zu characters long.\n", strings[n], my_strlen(strings[n]));
+  }
+
+  return 0;
+}
+
+size_t my_strlen(char* str) {
+  size_t n = 0;
+  while (str[++n]); /* noop, n is incremented within the condition */
+  return n;
+}
